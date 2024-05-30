bd6bc18240086bfda0c65dde21b90b3e4c3ae175
André Ximenes <andre1ximenes92@gmail.com>
2024-05-30 14:37:32 -0300

    Projeto *Lista de Tarefas* html e css

--- Javascript/aula19/js/aula19.js
+++ Javascript/aula19/js/aula19.js
@@ -17,9 +17,11 @@ console.log(obj.modelo);
 //FAZENDO UMA REQUISIÇÃO REMOTA PARA UM SITE
 const ajax = new XMLHttpRequest();
 
-ajax.open('GET', 'https://viacep.com.br/ws/01001000/json/');
+ajax.open('GET', 'https://viacep.com.br/ws/01001000/json/'); //pega a requisição
 
-ajax.send();
+ajax.send(); //envia uma requisição
+
+//carrega/ recebe a RESPOSTA (this.responseText) da requisição enviada
 
 // ajax.onload = function(){
 //     document.getElementById("area").innerHTML = this.responseText;
@@ -31,24 +33,30 @@ ajax.send();
 
 //outra maneira
 function buscarCep(){
-    let campoTexto = document.getElementById("cep").value;
+    let campoTexto = document.getElementById("cep").value; //pega o valor do input
 
+    //faz a requisição e envia o pedido
     const ajax = new XMLHttpRequest();
     ajax.open('GET', 'https://viacep.com.br/ws/'+ campoTexto + '/json/');
-
     ajax.send();
 
+    //carrega a resposta recebida
     ajax.onload = function(){
-
-        document.getElementById("texto"). innerHTML = this.responseText;
-
-        //filtrando para que seja visualizado apenas os dados importantes
-        let obj = JSON.parse(this.responseText); //converte para objeto
-        //peguei os valores que eu queria
-        let logradouro = obj.logradouro;
-        let cidade = obj.localidade;
-        let estado = obj.uf;
-
-        document.getElementById("texto"). innerHTML = "logradouro: " + logradouro + "<br>Cidade: " + cidade + "<br>Estado: " + estado;
+        let obj = JSON.parse(this.responseText);
+    
+        // Verifica se a propriedade 'erro' existe no objeto e se é verdadeira
+        if(obj.erro === true){
+            document.getElementById("texto").innerHTML = "Ocorreu um erro na solicitação. Não foi possível localizar esse CPF";
+        } 
+        else
+        {
+            let logradouro = obj.logradouro;
+            let cidade = obj.localidade;
+            let estado = obj.uf;
+    
+            document.getElementById("texto").innerHTML = "logradouro: " + logradouro + "<br>Cidade: " + cidade + "<br>Estado: " + estado;
+            
+        }
     }
+    
 }
